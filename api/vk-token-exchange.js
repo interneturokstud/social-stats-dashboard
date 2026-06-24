53835301b05d16a292bf8e1e2cc9f40779449df3
interneturokstud <iozhickov1313@gmail.com>
2026-06-24 12:49:01 +0300

    Update vk-token-exchange.js

--- api/vk-token-exchange.js
+++ api/vk-token-exchange.js
@@ -85,7 +85,7 @@ export default async function handler(req, res) {
   authUrl.searchParams.set('redirect_uri', WORKING_REDIRECT_URI);
   authUrl.searchParams.set('response_type', 'code');
   authUrl.searchParams.set('code_challenge', challenge);
-  authUrl.searchParams.set('code_challenge_method', 's256');
+  authUrl.searchParams.set('code_challenge_method', 'S256');
   authUrl.searchParams.set('scope', 'video');
   authUrl.searchParams.set('state', 'exchange');
   authUrl.searchParams.set('v', '5.199');
